8b5ecb0d9b6013ec14a495d17fc1b650d6dceb4d
Nikolai Kuropatkin <nikolai.kuropatkin@chdp-tech.net>
2023-09-01 16:37:15 +0200

    version 0.0.1 of the .so file was created

--- cmd/mtso.go
+++ cmd/mtso.go
@@ -1,11 +1,62 @@
 package main
 
 import "C"
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/seehuhn/mt19937"
+)
 
 //export SayHello
 func SayHello(name string) {
 	fmt.Println("Hello,", name)
+}
+
+// GenerateRandomTokens generates random tokens
+// rangeGen - amount of the tokens
+// tokensSlice - slice of the generated tokens
+func GenerateRandomTokens(rangeGen uint8) (tokensSlice []uint64) {
+	rng := mt19937.New()
+	// calculate the range from min to max by % from the cents
+	// generate x random tokens like a 100% of the range
+	tokensSlice = make([]uint64, rangeGen)
+	for i := uint8(0); i < rangeGen; i++ {
+		tokensSlice[i] = rng.Uint64()
+	}
+
+	return tokensSlice
+}
+
+// GetRandomIndex retrieves the randomly selected index
+func GetRandomIndex(tokensSlice []uint64) (randomIndex int) {
+	rng := rand.New(mt19937.New())
+	rng.Seed(time.Now().UnixNano())
+	randomIndex = rng.Intn(len(tokensSlice))
+
+	return randomIndex
+}
+
+// GetRandomNumber retrieves the randomly selected number
+func GetRandomNumber(tokensSlice []uint64) (randomNumber uint64) {
+	randomIndex := GetRandomIndex(tokensSlice)
+	// Retrieve the randomly selected number
+	randomNumber = tokensSlice[randomIndex]
+
+	return randomNumber
+}
+
+// DropPersonalBonus drops the personal bonus
+func DropPersonalBonus(rangeGen uint8) bool {
+	tokensSlice := GenerateRandomTokens(rangeGen)
+
+	randomNumber1 := GetRandomNumber(tokensSlice)
+	randomNumber2 := GetRandomNumber(tokensSlice)
+	if randomNumber2 == randomNumber1 {
+		return true
+	}
+	return false
 }
 
 func main() {}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gitlabce.champion.tm/champion-prj/mersenne-twister-shared-object
 
 go 1.21
+
+require github.com/seehuhn/mt19937 v1.0.0
