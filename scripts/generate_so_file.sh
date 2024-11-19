7d482f62822aeba8f2554a7ceb99096f5b8eb66c
Николай Куропаткин <nictaporuk@gmail.com>
2024-11-19 17:05:40 +0100

    develop: fix building a .so file for debian linux amd64 by docker

--- scripts/generate_so_file.sh
+++ scripts/generate_so_file.sh
@@ -4,10 +4,10 @@
 # TODO: and take .so file coz of the container name
 # TODO: make a cache for the image
 
-docker build --no-cache -t mtso:0.0.3 . -f ./docker/deb/Dockerfile
+docker build --no-cache -t mtso:0.0.4 . -f ./docker/deb/Dockerfile
 
 # Start a container in the background
-docker run -d --name mt_db mtso:0.0.3
+docker run -d --name mt_db mtso:0.0.4
 
 # Copy the built .so file from the container to the local bin directory
 docker cp mt_db:/app/bin/mt_deb.so ./bin/
